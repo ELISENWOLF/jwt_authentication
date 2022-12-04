f1d00d82eabd76df9ff8f9a7d719d989b5544304
ELISENWOLF <aravindprakash2001@gmail.com>
2022-12-04 21:51:06 +0530

    Data-Security-in-React

--- src/Component/Users.js
+++ src/Component/Users.js
@@ -18,8 +18,9 @@ const Users = () => {
                 const response = await axiosPrivate.get('/users', {
                     signal: controller.signal
                 });
+                const userNames = response.data.map(user => user.username)
                 console.log(response.data);
-                isMounted && setUsers(response.data);
+                isMounted && setUsers(userNames);
             } catch (err) {
                 console.error(err);
                 navigate('/login', { state: { from: location }, replace: true });
@@ -40,7 +41,7 @@ const Users = () => {
             {users?.length
                 ? (
                     <ul>
-                        {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+                        {users.map((user, i) => <li key={i}>{user}</li>)}
                     </ul>
                 ) : <p>No users to display</p>
             }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./App.css";
-import App from "./App";
 import { AuthProvider } from "./Component/Context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import "./App.css";
+import App from "./App";
+
+if(process.env.NODE_ENV === 'production'){
+  disableReactDevTools();
+}
 
 ReactDOM.render(
   <React.StrictMode>
